0dd2482751dcc9a8bc83114f5c9fc6d9795d66d1
SVss <savionak@gmail.com>
2016-12-01 10:22:28 +0300

    Normal input and error handling

--- CalculatorWcf/CalcClientConsole/Program.cs
+++ CalculatorWcf/CalcClientConsole/Program.cs
@@ -9,35 +9,72 @@ namespace CalcClientConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter your expression:");
-            string input = Console.ReadLine();
+            string input = PromptReadResult();
 
-            Parser parser = new Parser(input);
-            List<ExpressionItem> expr = parser.GetPostfixNotation();
+            while (input != "q")
+            {
+                try
+                {
+                    Parser parser = new Parser(input);
+                    List<ExpressionItem> expr = parser.GetPostfixNotation();
 
-            // Usual Expressions
+                    // Usual Expressions
 
-            var defaultDelegateBuilder = new DefaultDelegateBuilder();
-            Delegate del = defaultDelegateBuilder.GetDelegate(expr);
+                    var defaultDelegateBuilder = new DefaultDelegateBuilder();
+                    Delegate del = defaultDelegateBuilder.GetDelegate(expr);
 
-            object result = del.DynamicInvoke();
+                    object result = del.DynamicInvoke();
+                    WriteResult(result, "Local result");
+
+                    // WCF = Expression.Call(...)
+
+                    var wcfDelegateBuilder = new WcfDelegateBuilder(new CalcServiceClient());
+                    del = wcfDelegateBuilder.GetDelegate(expr);
+
+                    result = del.DynamicInvoke();
+                    WriteResult(result, "WCF result");
+                }
+                catch (InvalidExprException)
+                {
+                    Console.WriteLine(Messages.IncorrectExpression);
+                }
+                catch (InvalidBracketsException)
+                {
+                    Console.WriteLine(Messages.IncorrectBrackets);
+                }
+
+                Console.WriteLine();
+                input = PromptReadResult();
+            }
+        }
+
+        private static string PromptReadResult(string prompt = "Enter expression:")
+        {
+            Console.WriteLine("Enter your expression (or 'q' to exit):");
+            return Console.ReadLine();
+        }
+
+        private static void WriteResult(object result, string msg = "Результат")
+        {
             if (result is Double)
             {
-                Console.WriteLine($"Local result: {result}");
+                double val = (double) result;
+                if (double.IsNaN(val) || double.IsInfinity(val))
+                {
+                    Console.WriteLine($"{msg}: {Messages.ZeroDivide}");
+                }
+                else
+                {
+                    Console.WriteLine($"{msg}: {result}");
+                }
             }
+        }
 
-            // WCF = Expression.Call(...)
-
-            var wcfDelegateBuilder = new WcfDelegateBuilder(new CalcServiceClient());
-            del = wcfDelegateBuilder.GetDelegate(expr);
-
-            result = del.DynamicInvoke();
-            if (result is Double)
-            {
-                Console.WriteLine($"WCF result: {result}");
-            }
-
-            Console.ReadKey();
+        private static class Messages
+        {
+            public static string ZeroDivide => "Деление на ноль";
+            public static string IncorrectBrackets => "Неверно расставлены скобки";
+            public static string IncorrectExpression => "Неверное выражение";
         }
     }
 }
